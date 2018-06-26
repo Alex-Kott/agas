e7d8ab192d9d95df835356dff93def1aa8913080
Alexey Kott <alexey.kott@gmail.com>
2018-06-26 10:29:00 +0300

    ...

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # coding=utf-8
 import re
+import sys
 
 from openpyxl import load_workbook
 from pathlib import Path
@@ -14,11 +15,11 @@ def is_interesting(tr, azs_prefixes):
     return False
 
 
-def parse_report():
+def parse_report(file_name):
     azs_prefixes = ['ALK', 'CHO', 'EKA', 'HMO', 'IVO', 'KDK', 'KEO', 'KYK', 'MSK', 'NNO', 'NSO',
                     'OMO', 'SPB', 'SVO', 'TUO', 'YAR', 'IAR', 'KRR', 'MOW', 'MSK', 'NN', 'RYZ',
                     'CEK', 'MSK', 'NN', 'SPB', 'TUO', 'YAR']
-    with open("AGAS.html") as file:
+    with open(str(file_name)) as file:
         soup = BeautifulSoup(file.read(), "lxml")
 
     tbody = soup.find("tbody")
@@ -48,12 +49,30 @@ def is_empty(s, n):
     return True
 
 
+def parse_arguments(arguments):
+    if len(arguments) != 3:
+        raise Exception("Must be 2 argument (.html-file and .xlsx-file)")
+
+    args = [Path(arg) for arg in arguments]
+
+    for arg in args:
+        if arg.suffix == ".html":
+            agas_file_name = arg
+        elif arg.suffix == ".xlsx":
+            order_file_name = arg
+        elif arg.suffix == ".py":
+            pass
+        else:
+            raise Exception("Unknown file")
+
+    return agas_file_name, order_file_name
+
+
 if __name__ == "__main__":
-    cwd = Path(".")
-    xls_files = [entry for entry in cwd.iterdir() if entry.suffix == ".xlsx"]
-    report = parse_report()
-    order_file_name = "order_agas_may_june.xlsx"
-    wb = load_workbook(order_file_name)
+    (agas_file_name, order_file_name) = parse_arguments(sys.argv)
+
+    report = parse_report(agas_file_name)
+    wb = load_workbook(str(order_file_name))
     ws = wb.active
 
     flag = False
@@ -67,14 +86,26 @@ if __name__ == "__main__":
             continue
 
         azs_code = row[1].value
+        val = 0
         if type(row[12].value) is long:
             n = row[12].value
-            row16 = report[azs_code] - n
-        if row16 < 0:
-            row[16].fill = PatternFill(fill_type='solid',
-                                       start_color='FF9900',
-                                       end_color='FF9900')
+            try:
+                row16 = report[azs_code] - n
+                val = report[azs_code]
+                row[15].value = val
+                if row16 < 0:
+                    row[16].fill = PatternFill(fill_type='solid',
+                                               start_color='FF9900',
+                                               end_color='FF9900')
+            except KeyError as e:
+                row[15].value = "Данных нет"
+        elif type(row[12].value) is str:
+            row[15].value = val
 
-        row[15].value = report[azs_code]
+                
 
-    wb.save('test.xlsx')
+
+
+
+
+    wb.save('file.xlsx')
